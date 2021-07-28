23abc0720cf83aed7f22103077dc29475d90c9c0
zuha-khalid-au3 <zuhakhalid@gmail.com>
2021-07-28 12:16:01 +0530

    Post Create

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+export default ({ postId }) => {
+  return <div>
+<form>
+    <div className="form-group">
+        <label
+    </div>
+</form>
+  </div>;
+};
