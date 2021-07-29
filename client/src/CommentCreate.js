a1bfb76b78d4be73f552cf3fa637f1867ad14a04
zuha-khalid-au3 <zuhakhalid@gmail.com>
2021-07-29 12:17:53 +0530

    Comment Functionality Added

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,12 +1,28 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 export default ({ postId }) => {
-  return <div>
-<form>
-    <div className="form-group">
-        <label
+  const [content, setContent] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+    setContent("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New comment</label>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-</form>
-  </div>;
+  );
 };
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   const [posts, setPosts] = useState({});
@@ -19,6 +21,8 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
